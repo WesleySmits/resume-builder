7b8fa5d60c678965e5060c07c717153838c58925
Wesley Smits <wesleysmits@icloud.com>
2025-01-01 14:52:36 +0300

    Enhance IntroductionPage tests: add PNG support for profile images, handle empty images, and validate topSkills rendering

--- src/utils/__tests__/IntroductionPage.spec.ts
+++ src/utils/__tests__/IntroductionPage.spec.ts
@@ -28,7 +28,7 @@ vi.mock('@/stores/resume', () => {
             drivingLicense: 'Car',
             functionTitle: 'Watch Commander',
             introduction: 'I am the sword in the darkness.',
-            achievements: ['Defeated the Night King', 'Knows nothing', 'King in the North'],
+            achievements: ['Knows nothing', '', 'King in the North \n\n Defeated the Night King'],
             colleaguesDescribe: 'Brave',
             colleaguesKnow: 'Loyal',
         },
@@ -79,6 +79,8 @@ describe('IntroductionPage', () => {
         };
 
         pdfDoc.embedJpg = vi.fn().mockResolvedValue([1, 2, 3]);
+        pdfDoc.embedPng = vi.fn().mockResolvedValue([1, 2, 3]);
+
         pdfDoc.addPage = vi.fn().mockReturnValue(page);
 
         return pdfDoc;
@@ -126,6 +128,7 @@ describe('IntroductionPage', () => {
 
     it('should render correct data on the left column', async () => {
         const introductionPage = IntroductionPage.getInstance();
+        const originalDrawRightColumn = introductionPage.drawRightColumn;
         introductionPage.drawRightColumn = vi.fn();
 
         const store = useResumeStore();
@@ -158,10 +161,182 @@ describe('IntroductionPage', () => {
         expect(page.drawText).toHaveBeenCalledWith(store.topSkills[1].name, expect.any(Object));
 
         expect(page.drawRectangle).toHaveBeenCalledTimes(store.topSkills.length * 2);
+
+        introductionPage.drawRightColumn = originalDrawRightColumn;
     });
 
-    it('should render correct data on the right column', () => {
-        expect(1).toBe(1);
+    it('should render the profile photo on the left column as a PNG', async () => {
+        const introductionPage = IntroductionPage.getInstance();
+        const originalDrawRightColumn = introductionPage.drawRightColumn;
+        introductionPage.drawRightColumn = vi.fn();
+
+        const store = useResumeStore();
+        store.general.profilePhoto = 'data:image/png;base64,';
+
+        const pdfDoc = await getPdfDocumentWithMocks();
+        await introductionPage.initialize(pdfDoc);
+
+        const page = pdfDoc.addPage();
+
+        expect(page.drawImage).toHaveBeenCalledOnce();
+        expect(page.drawImage).toHaveBeenCalledWith([1, 2, 3], {
+            x: 60,
+            y: 601.89,
+            width: 140,
+            height: 140,
+        });
+
+        introductionPage.drawRightColumn = originalDrawRightColumn;
+    });
+
+    it('should throw an error if the file type is not recognizes for the profile image', async () => {
+        const introductionPage = IntroductionPage.getInstance();
+        const originalDrawRightColumn = introductionPage.drawRightColumn;
+        introductionPage.drawRightColumn = vi.fn();
+
+        const store = useResumeStore();
+        store.general.profilePhoto = 'data:image/WRS;base64,';
+
+        const pdfDoc = await getPdfDocumentWithMocks();
+
+        await expect(async () => {
+            await introductionPage.initialize(pdfDoc);
+        }).rejects.toThrow();
+
+        introductionPage.drawRightColumn = originalDrawRightColumn;
+    });
+
+    it('should not draw the profile image if the profile image is empty', async () => {
+        const introductionPage = IntroductionPage.getInstance();
+        const originalDrawRightColumn = introductionPage.drawRightColumn;
+        introductionPage.drawRightColumn = vi.fn();
+
+        const store = useResumeStore();
+        store.general.profilePhoto = '';
+
+        const pdfDoc = await getPdfDocumentWithMocks();
+
+        await introductionPage.initialize(pdfDoc);
+
+        const page = pdfDoc.addPage();
+        expect(page.drawImage).not.toHaveBeenCalled();
+
+        introductionPage.drawRightColumn = originalDrawRightColumn;
+    });
+
+    it('should render introduction correctly on the right column', async () => {
+        const introductionPage = IntroductionPage.getInstance();
+        const backupDrawLeftColumn = introductionPage.drawLeftColumn;
+        introductionPage.drawLeftColumn = vi.fn();
+
+        const store = useResumeStore();
+        const introText =
+            "I am the sword in the darkness. I am the watcher on the walls. I am the shield that guards the realms of men. I pledge my life and honor to the Night's Watch, for this night and all the nights to come. \n\n I am the sword in the darkness. I am the watcher on the walls. I am the shield that guards the realms of";
+        store.general.introduction = introText;
+        const pdfDoc = await getPdfDocumentWithMocks();
+        await introductionPage.initialize(pdfDoc);
+
+        const page = pdfDoc.addPage();
+
+        await nextTick();
+
+        expect(page.drawText).toHaveBeenCalledWith(
+            'I am the sword in the darkness. I am the watcher on the walls. I am',
+            expect.any(Object),
+        );
+
+        introductionPage.drawLeftColumn = backupDrawLeftColumn;
+    });
+
+    it('should not draw the topSkills if they are empty', async () => {
+        const introductionPage = IntroductionPage.getInstance();
+        const originalDrawRightColumn = introductionPage.drawRightColumn;
+        introductionPage.drawRightColumn = vi.fn();
+
+        const store = useResumeStore();
+        store.topSkills = [];
+
+        const pdfDoc = await getPdfDocumentWithMocks();
+
+        await introductionPage.initialize(pdfDoc);
+
+        const page = pdfDoc.addPage();
+        expect(page.drawText).not.toHaveBeenCalledWith('Skills (experience in years)', expect.any(Object));
+
+        introductionPage.drawRightColumn = originalDrawRightColumn;
+    });
+
+    it('should draw the topSkills if they are set', async () => {
+        const introductionPage = IntroductionPage.getInstance();
+        const originalDrawRightColumn = introductionPage.drawRightColumn;
+        introductionPage.drawRightColumn = vi.fn();
+
+        const store = useResumeStore();
+        store.topSkills = [
+            {
+                name: 'JavaScript',
+                yearsOfExperience: 10,
+            },
+            {
+                name: 'TypeScript',
+                yearsOfExperience: 6,
+            },
+            {
+                name: 'Vue.js',
+                yearsOfExperience: 5,
+            },
+            {
+                name: 'React',
+                yearsOfExperience: 5,
+            },
+            {
+                name: 'Angular',
+                yearsOfExperience: 5,
+            },
+            {
+                name: 'Node.js',
+                yearsOfExperience: 5,
+            },
+            {
+                name: 'Express.js',
+                yearsOfExperience: 5,
+            },
+            {
+                name: 'MongoDB',
+                yearsOfExperience: 5,
+            },
+            {
+                name: 'PostgreSQL',
+                yearsOfExperience: 5,
+            },
+            {
+                name: 'Docker',
+                yearsOfExperience: 0,
+            },
+            {
+                name: 'Kubernetes',
+                yearsOfExperience: 5,
+            },
+            {
+                name: 'Jest',
+                yearsOfExperience: 5,
+            },
+        ];
+
+        const pdfDoc = await getPdfDocumentWithMocks();
+
+        await introductionPage.initialize(pdfDoc);
+
+        const page = pdfDoc.addPage();
+        expect(page.drawText).toHaveBeenCalledWith('JavaScript', expect.any(Object));
+
+        // Should not render more than MAX_SKILLS (f.e. 10) top skills
+        expect(page.drawText).not.toHaveBeenCalledWith('Jest', expect.any(Object));
+
+        // Should not render skills with 0 years of experience
+        expect(page.drawText).not.toHaveBeenCalledWith('Docker', expect.any(Object));
+
+        introductionPage.drawRightColumn = originalDrawRightColumn;
     });
 
     describe('test empty store', () => {
